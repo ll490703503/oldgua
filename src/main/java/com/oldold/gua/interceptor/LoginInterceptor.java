9e1c76ee7a97ae4b74a97199035071faa40e01aa
liule <liule0814@163.com>
2019-02-22 11:58:22 +0800

    last

--- src/main/java/com/oldold/gua/interceptor/LoginInterceptor.java
+++ src/main/java/com/oldold/gua/interceptor/LoginInterceptor.java
@@ -22,7 +22,7 @@ public class LoginInterceptor implements HandlerInterceptor {
         if (session.getAttribute("user") == null) {
             if ("XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))) {
                 response.sendError(401);
-
+                System.out.println("hahha");
             }
             response.sendRedirect("/");
             return false;
