2dae414c37ec8c5311383a3ee50b4fd5a86a2540
admin <liule0814@163.com>
2019-02-23 17:04:16 +0800

    test

--- src/main/java/com/oldold/gua/controller/Usercon.java
+++ src/main/java/com/oldold/gua/controller/Usercon.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * @Description
